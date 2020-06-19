62ff479234fbe6d098a8dfa8f3e6cf39ca7f1c5a
Freet8707 <freet8707@gmail.com>
2020-06-18 22:24:33 -0500

    header nav font selected and styled with display: flex

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import './App.css';
+
+const HeaderDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  max-width: 100%;
+  height: 50px;
+  padding: 0 25px 5px 25px;
+  background: rgb(232, 76, 61);
+  h1 {
+    font-family: 'Ubuntu', sans-serif;
+    color: rgb(243, 243, 243);
+    font-size: 4rem;
+  }
+`;
 
 function NavBar() {
   return (
-    <>
+    <HeaderDiv>
       <h1>African Marketplace</h1>
-      <Link to='/'><button>Home</button></Link>
-      <Link to='/about'><button>About</button></Link>
-    </>
+      <div>
+        <Link to='/'><button>Home</button></Link>
+        <Link to='/about'><button>About</button></Link>
+      </div>
+    </HeaderDiv>
   )
 }
 
