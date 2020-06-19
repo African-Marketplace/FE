69c232c963577cbbbb04e149d2d9a41b864b6f4a
Freet8707 <freet8707@gmail.com>
2020-06-18 22:09:00 -0500

    added styled-components and started a skeleton structure for a nav header

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, Route } from 'react-router-dom';
 import './App.css';
+
+function NavBar() {
+  return (
+    <>
+      <h1>African Marketplace</h1>
+      <Link to='/'><button>Home</button></Link>
+      <Link to='/about'><button>About</button></Link>
+    </>
+  )
+}
 
 function App() {
   return (
     <div>
+      <NavBar />
     </div>
   );
 }
