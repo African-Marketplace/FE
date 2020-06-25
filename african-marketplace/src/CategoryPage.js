d52fe3e5929a13c730e4678c4da7df0f58cf31b1
Freet8707 <freet8707@gmail.com>
2020-06-24 19:46:17 -0500

    further style changes

--- african-marketplace/src/CategoryPage.js
+++ african-marketplace/src/CategoryPage.js
@@ -5,16 +5,16 @@ function CategoryPage(props) {
     const params = useParams();
     console.log(params)
 
-    useEffect(() => {
-      const appStyle = document.querySelector('.auth-wrapper')
+    // useEffect(() => {
+    //   const appStyle = document.querySelector('.auth-wrapper')
       
-      appStyle.classList.add('authToggle')
-    }, [])
+    //   appStyle.classList.add('authToggle')
+    // }, [])
 
     const category = props.category.find(item => item.name === params.catName)
 
     return (
-        <div style={{position: 'relative', marginTop: '100px'}}>
+        <div style={{marginTop: '100px'}}>
           <p style={{color: 'white', fontSize: '2rem'}}>{category.name}</p>
           <Link to='/itemlist'><button>Back to Categories</button></Link>  
         </div>
